b13cc38d828dcbd2a4a347f1e788d5932e131dbb
nishitjoshi1617 <nishitjoshi2008@gmail.com>
2026-02-23 15:27:56 +0530

    Create error.py

---
+++ error.py
@@ -0,0 +1,17 @@
+PROGRAM 61 — | Enrolment: 241105060061
+print("Courier Booking")
+cities = ["Delhi","Mumbai","Pune"]
+charges = [100,150,120]
+from_city = input("From city: ")
+to_city = input("To city: ")
+if from_city in cities and to_city in cities
+ weight = input("Enter weight: ")
+ amount = weight * 50
+else
+ print("City not available")
+print("From:", from_city)
+print("To:", to_city)
+print("Amount:", Amount)
+for i in range(3)
+print("Booked")
+print("End")
